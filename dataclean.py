3355c09e4d3a1d6e37139f1ec6bca30831d97503
aizhou zou <azou@vols.utk.edu>
2021-04-23 20:51:07 -0400

    combine code

--- dataclean.py
+++ dataclean.py
@@ -9,28 +9,37 @@ from dash.exceptions import PreventUpdate
 import matplotlib.pyplot as plt
 
 
+df = pd.read_csv("games_flat_xml_2012-2018.csv") 
+
+# convert temp column
+df['temp'] = df['temp'].fillna(0)
+df['temp'] = df['temp'].replace({"Indoor": "72","Indoors":"72","N/A/":"0"})
+df['temp'] = df['temp'].to_list()
+df['temp'] = [int(str(s)[:2]) for s in df['temp'] ]
 
 
-SA = pd.read_csv("games_flat_xml_2012-2018.csv")      
+##convert date column
+df['date'] = pd.to_datetime(df['date'])
 
-SA['postseason']=  SA.postseason.apply(lambda x :1 if x == 'Y' else '0')
+
+#convert weather to binary if rain or not
+df['postseason']=  df.postseason.apply(lambda x :1 if x == 'Y' else '0')
 # Y => 1, character(0) => 0
-SA['neutralgame']=  SA.neutralgame.apply(lambda x :1 if x == 'Y' else '0')  # Y => 1, character(0) => 0
-
-# convert duration to minutes
-SA['duration'] = SA['duration'].apply(lambda x: int(x[0]) * 60 + int(x[-2:]))
-
-plt.scatter(SA['neutralgame'], SA['attend'])
+df['neutralgame']=  df.neutralgame.apply(lambda x :1 if x == 'Y' else '0')  # Y => 1, character(0) => 0
+plt.scatter(df['neutralgame'], df['attend'])
 plt.show()
-
-
 #make weather binary: if rain then 1 else then 0
 x=0
-for i in SA['weather'] :
+for i in df['weather'] :
     if i in ['Showers','94% hum. 30% rain', 'Rainy','Cloudy, Light Rain','rain','Light Rain','Steady rain','Rain in the area.','Cloudy with rain','Nice, rain in area','showers, 90% humid.','Cloudy, 25% Rain','lt. rain, 75% humid.','scattered', 'Light rain, Overcast','Cloudy rain possible','Light rain,87% humid','light rain','Cloudy/Rain','Cloudy, Drizzly','cloudy, light rain','Rain in the area','Overcast, light rain','Rain','Drizzly','Light rain','Chance of rain','74% humid., lt.rain','RAINY','40% chance of rain','Cloudy, light rain','Cloudy & Rain','Intermittent Rain','Cloudy possible rain','Intermittent Showers']:
-        SA['weatherb'].iloc[x] = 1 
+        df['weatherb'].iloc[x] = 1 
      else: 
-        SA['weatherb'].iloc[x] = 0
+        df['weatherb'].iloc[x] = 0
     x=x+1
-  
-SA['weather'].value_counts().head(30)
+df['weather'].value_counts().head(30)
+df['attend'].describe()
+
+
+#merging 2 tables 
+df1 = pd.read_csv("TV_Ratings_onesheet.csv")
+df2 = pd.merge(df, df1, on="TeamIDsDate")
